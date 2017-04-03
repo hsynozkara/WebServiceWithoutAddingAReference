c34ab771727041a266c8353fc3400b402398526e
Hüseyin Özkara <hsyn.ozkara@gmail.com>
2017-04-03 16:16:52 +0300

    Add project files.

---
+++ ConsoleWebServiceApplication/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleWebServiceApplication
+{
+    internal class Customer
+    {
+        public string City { get; set; }
+        public string CompanyName { get; set; }
+        public string CustomerID { get; set; }
+    }
+}
---
+++ ConsoleWebServiceApplication/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWebServiceApplication
+{
+    class Program
+    {
+      
+
+        public static string CallWebService(string URL)
+        {
+            HttpWebRequest objRequest = (HttpWebRequest)WebRequest.Create(URL);
+            objRequest.Method = "GET";
+            objRequest.KeepAlive = false;
+            objRequest.UseDefaultCredentials = true;
+
+            HttpWebResponse objResponse = (HttpWebResponse)objRequest.GetResponse();
+           
+            string result = "";
+            using (StreamReader sr = new StreamReader(objResponse.GetResponseStream()))
+            {
+                result = sr.ReadToEnd();
+                sr.Close();
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string JSONresponse = CallWebService("http://www.inorthwind.com/Service1.svc/getAllCustomers");
+
+            List<Customer> customers = JsonConvert.DeserializeObject<List<Customer>>(JSONresponse);
+        }
+
+
+    }
+}
